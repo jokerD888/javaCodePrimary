31573d02c6fafa4a934625f6b193be8ac005ee9a
joker <1783779005@qq.com>
2021-07-06 21:59:40 +0800

    Hello Java!

---
+++ Hello.java
@@ -0,0 +1,18 @@
+
+
+//java快速入门,演示java的快速开发
+//对代码的相关说明
+//1.public class Hello 表面Hello是一个类，是一个public公有的类
+//2.Hello{}表示一个类的开始和结束
+//3.public static void main(String[] args)表示一个主方法，即程序的入口
+//4.main()表示方法的开始和结束
+//
+
+public class Hello
+{
+	public static void main(String[] args)
+	{
+		System.out.println("hello world~");//输出hello world~到屏幕
+	}
+}
+
---
+++ laowang.java
@@ -0,0 +1,7 @@
+public class laowang
+{
+	public static void main(String[] args)
+	{
+		System.out.println("老王 is studying java!");//输出hello world~到屏幕
+	}
+}
